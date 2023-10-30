3c6fb976b44929763849e1df97cda692a3f7cdee
saieswar90 <saieswarreddy321@gmail.com>
2023-10-30 11:56:16 +0530

    changes done

--- DEMOPUSH/src/pack1/Firstp.java
+++ DEMOPUSH/src/pack1/Firstp.java
@@ -1,10 +1,7 @@
 package pack1;
-
 public class Firstp {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("Hello CSD");
 	}
-
 }
