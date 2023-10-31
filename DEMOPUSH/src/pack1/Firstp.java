9f37220953de258930320bb445c7a923c09f7314
saieswar90 <saieswarreddy321@gmail.com>
2023-10-31 14:13:51 +0530

    commit done

--- DEMOPUSH/src/pack1/Firstp.java
+++ DEMOPUSH/src/pack1/Firstp.java
@@ -1,13 +1,10 @@
 package pack1;
-import java.util.*;
 public class Firstp {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Hello world");
 		System.out.println("Hello CSD");
 		System.out.println("Hello ECS");
-		int a=sc.nextInt();
-		System.out.print(a);
 		System.out.println("Hello sai");
 	}
 }
