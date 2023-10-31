80dc41fe3302d66b9147d3204830d8020b1c2112
saieswar90 <saieswarreddy321@gmail.com>
2023-10-31 14:22:02 +0530

    changes done again

--- DEMOPUSH/src/pack1/Firstp.java
+++ DEMOPUSH/src/pack1/Firstp.java
@@ -1,7 +1,6 @@
 package pack1;
 public class Firstp {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
 		System.out.println("Hello world");
 		System.out.println("Hello CSD");
 		System.out.println("Hello ECS");
